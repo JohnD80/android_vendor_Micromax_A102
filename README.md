98544546a6ec2e307b73a9ea82615b74e4e0bb99
kirito96 <eagle1968@live.com>
2017-12-26 13:55:49 -0400

    Add cm13 files

---
+++ README.md
@@ -0,0 +1,2 @@
+# android_vendor_TCT_Yaris_M_GSM
+This vendor tree is for TCT Yaris_M_GSM,MT6572.
--- Yaris_M_GSM-vendor.mk
+++ Yaris_M_GSM-vendor.mk
@@ -1,8 +1,8 @@
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCT/Yaris_M_GSM/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCT/Yaris_M_GSM/proprietary/etc,system/etc)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCT/Yaris_M_GSM/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCT/Yaris_M_GSM/proprietary/etc,system/etc)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/TCT/Yaris_M_GSM/proprietary/vendor,system/vendor)
 
